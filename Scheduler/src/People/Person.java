6c9253b09671d144d092531f02483ed4deede754
Tyler Leite <t.leite1995@gmail.com>
2013-11-26 16:28:26 -0500

    Update Person.java

--- Scheduler/src/People/Person.java
+++ Scheduler/src/People/Person.java
@@ -7,7 +7,15 @@ public abstract class Person {
     public Schedule sch = new Schedule();
     public String name; // Full name, (First M Last)
     
+    public int coursesToSchedule = 0;
+    public int coursesLeftToSchedule = 0;
+    
     public Person(String name){
         this.name = name;
+    }
+    
+    public void addCoursesToSchedule(int freq){
+    	coursesToSchedule += freq;
+    	coursesLeftToSchedule += freq;
     }
 }
