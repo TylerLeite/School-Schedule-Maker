ea1d324cd86c9bd062e04dc7c792b5a98fb8d66f
Tyler Leite <t.leite1995@gmail.com>
2013-11-29 14:28:08 -0500

    Update Schedule.java

--- Scheduler/src/Schedule/Schedule.java
+++ Scheduler/src/Schedule/Schedule.java
@@ -3,12 +3,11 @@ package Schedule;
 import java.util.HashSet;
 import java.util.ArrayList;
 
-
 import Course.Course;
 
 
 public class Schedule {
-    public static final int T = 9; // 9 periods per day (excluding Snack)
+    public static final int T = 12; // 9 periods per day (Excluding Snack)
     public static final int D = 5; // Approximately 5 school days per week
     public static final int LUNCH = 5;
     
@@ -32,13 +31,13 @@ public class Schedule {
         openings = new HashSet<Opening>();
         for (int i = 0; i < D; i++){
             for (int j = 0; j < T; j++){
-            	if (!schedule[i][j].equals("DNF")){
-            		schedule[i][j] = "";
+                if (!schedule[i][j].equals("DNF")){
+                    schedule[i][j] = "";
                     openings.add(new Opening(i, j));
-            	}
-            	
-            	if (j == LUNCH)
-        			schedule[i][j] = "Lunch";
+                }
+                
+                if (j == LUNCH)
+                    schedule[i][j] = "Lunch";
             }
         }
     }
@@ -52,14 +51,14 @@ public class Schedule {
     }
     
     public boolean scheduleCourse(int day, int time, Course course){
-    	/* Remove the opening in both the openings HashSet and schedule array. */
+        /* Remove the opening in both the openings HashSet and schedule array. */
         if (schedule[day][time].isEmpty() || schedule[day][time].equals("Lunch")){
             schedule[day][time] = course.getString(day);
             restrict(day, time);
             
             return true;
         } else {
-        	System.out.println("Schedule spot taken!");
+            System.out.println("Schedule spot taken!");
             return false;
         }
     }
@@ -85,14 +84,14 @@ public class Schedule {
     public void restrict(int day, int time){
         for (Opening opening : openings){
             if (opening.day == day && opening.time == time){
-	            openings.remove(opening);
-	            break;
+                openings.remove(opening);
+                break;
             }
         }
     }
     
     public void restrict(Opening opening){
-    	restrict(opening.day, opening.time);
+        restrict(opening.day, opening.time);
     }
     
     public void restrictTime(int time){
