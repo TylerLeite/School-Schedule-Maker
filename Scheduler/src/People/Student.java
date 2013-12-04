6f50cdca19f2671aaca51870e33b8cbaac34af11
Tyler Leite <t.leite1995@gmail.com>
2013-12-04 07:41:49 -0500

    removed superfluous files

--- Scheduler/src/People/Student.java
+++
@@ -1,9 +0,0 @@
-package People;
-
-public class Student extends Person {
-    /* No extra functionality as yet. */
-    
-    public Student(String name){
-        super(name);
-    }
-}
--- Scheduler/src/People/Teacher.java
+++
@@ -1,9 +0,0 @@
-package People;
-
-public class Teacher extends Person {
-    /* No extra functionality as yet. */
-    
-    public Teacher(String name){
-        super(name);
-    }
-}
