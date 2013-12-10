8601e068df38d22510c9b1e7e217927cf4626d33
Tyler Leite <t.leite1995@gmail.com>
2013-12-10 17:44:54 -0500

    Update Person.java
    Added support for new input

--- src/People/Person.java
+++ src/People/Person.java
@@ -6,6 +6,7 @@ import Schedule.Schedule;
 public abstract class Person {
     public Schedule sch = new Schedule();
     public String name; // Full name, (First M Last)
+    public HashMap<String><Integer> artsLevels = new HashMap<String><Integer>(); // Level of theater, dance, music, art
     
     public int totalCourses = 0;
     public int coursesLeftToSchedule = 0;
