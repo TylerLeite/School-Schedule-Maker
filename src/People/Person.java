fb90700ab1972ef834098d857cf12d343f1c6181
Tyler Leite <t.leite1995@gmail.com>
2013-12-10 17:47:47 -0500

    fixed syntax error

--- src/People/Person.java
+++ src/People/Person.java
@@ -1,12 +1,14 @@
 package People;
 
+import java.util.HashMap;
+
 import Schedule.Schedule;
 
 
 public abstract class Person {
     public Schedule sch = new Schedule();
     public String name; // Full name, (First M Last)
-    public HashMap<String><Integer> artsLevels = new HashMap<String><Integer>(); // Level of theater, dance, music, art
+    public HashMap<String, Integer> artsLevels = new HashMap<String, Integer>(); // Level of theater, dance, music, art
     
     public int totalCourses = 0;
     public int coursesLeftToSchedule = 0;
